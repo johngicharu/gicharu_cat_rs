bd2d19873b0f685976342851b870749c5854e19c
John Gicharu <johngicharu@ids.company>
2023-11-26 22:04:57 +0300

    feat: base solution with no support for non-utf8 encoded files + nowhitespace

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // Find all files in the arguments sent to the program
+    let passed_args: Vec<String> = env::args().collect();
+
+    // Whenever a file is found, add the file contents to the string (resulting string)
+    let paths = &passed_args[1..];
+
+    let mut result = String::new();
+
+    // Loop through each path and find the file
+    for path in paths.iter() {
+        match fs::read_to_string(path) {
+            Ok(res) => result.push_str(&res),
+            Err(err) => result.push_str(&err.to_string()),
+        }
+
+        // result.push_str("\r\n\r\n");
+    }
+
+    // Otherwise, if the file is not found, we continue to the next file and log an error for that specific file
+    println!("{}", result);
 }
